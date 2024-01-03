ba7513c50a9cef924c4b4245f2f6918aa1a77697
Sonia CHELLABI <chellabisonia@gmail.com>
2024-01-03 22:10:42 +0100

    AsyncStorage use

--- react-native-todolist/react-native-todolist/App.js
+++ react-native-todolist/react-native-todolist/App.js
@@ -1,20 +1,60 @@
-import { useState } from "react";
-import { ScrollView, Text, View, Alert } from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, ScrollView, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
+import { ButtonAdd } from "./components/ButtonAdd/ButtonAdd";
 import { CardTodo } from "./components/CardTodo/CardTodo";
 import { Header } from "./components/Header/Header";
 import { TabBottomMenu } from "./components/TabBottomMenu/TabBottomMenu";
-import { ButtonAdd } from "./components/ButtonAdd/ButtonAdd";
 import Dialog from "react-native-dialog";
 import uuid from "react-native-uuid";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+let isFirstRender = true;
+let isLoadUpdate = false;
 export default function App() {
   const [selectedTabName, setSelectedTabName] = useState("all");
   const [todoList, setTodoList] = useState([]);
-  const [isAddDialogVisible, setIsDialogVisible] = useState(false);
+  const [isAddDialogVisible, setIsAddDialogVisible] = useState(false);
   const [inputValue, setInputValue] = useState("");
-  console.log(inputValue);
+
+  useEffect(() => {
+    loadTodoList();
+  }, []);
+
+  useEffect(() => {
+    if (isLoadUpdate) {
+      isLoadUpdate = false;
+    } else {
+      if (!isFirstRender) {
+        saveTodoList();
+      } else {
+        isFirstRender = false;
+      }
+    }
+  }, [todoList]);
+  async function saveTodoList() {
+    console.log("SAVE");
+    try {
+      await AsyncStorage.setItem("@todolist", JSON.stringify(todoList));
+    } catch (err) {
+      alert("Erreur " + err);
+    }
+  }
+
+  async function loadTodoList() {
+    console.log("LOAD");
+    try {
+      const stringifiedTodoList = await AsyncStorage.getItem("@todolist");
+      if (stringifiedTodoList !== null) {
+        const parsedTodoList = JSON.parse(stringifiedTodoList);
+        isLoadUpdate = true;
+        setTodoList(parsedTodoList);
+      }
+    } catch (err) {
+      alert("Erreur " + err);
+    }
+  }
 
   function getFilteredList() {
     switch (selectedTabName) {
@@ -26,7 +66,6 @@ export default function App() {
         return todoList.filter((todo) => todo.isCompleted);
     }
   }
-
   function updateTodo(todo) {
     const updatedTodo = {
       ...todo,
@@ -40,19 +79,26 @@ export default function App() {
     const updatedTodoList = [...todoList];
     updatedTodoList[indexToUpdate] = updatedTodo;
     setTodoList(updatedTodoList);
+    shouldSave = true;
   }
+
   function deleteTodo(todoToDelete) {
-    Alert.alert("Supression", "Supprimer cette tâche?", [
+    Alert.alert("Suppression", " Supprimer cette tâche ?", [
       {
         text: "Supprimer",
         style: "destructive",
         onPress: () => {
+          shouldSave = true;
           setTodoList(todoList.filter((todo) => todo.id !== todoToDelete.id));
         },
       },
-      { text: "Annuler ", style: "cancel" },
+      {
+        text: "Annuler",
+        style: "cancel",
+      },
     ]);
   }
+
   function renderTodoList() {
     return getFilteredList().map((todo) => (
       <View style={s.cardItem} key={todo.id}>
@@ -61,18 +107,19 @@ export default function App() {
     ));
   }
   function showAddDialog() {
-    setIsDialogVisible(true);
+    setIsAddDialogVisible(true);
   }
+
   function addTodo() {
     const newTodo = {
       id: uuid.v4(),
       title: inputValue,
       isCompleted: false,
     };
+    shouldSave = true;
     setTodoList([...todoList, newTodo]);
-    setIsDialogVisible(false)
+    setIsAddDialogVisible(false);
   }
-
   return (
     <>
       <SafeAreaProvider>
@@ -93,9 +140,7 @@ export default function App() {
       />
       <Dialog.Container
         visible={isAddDialogVisible}
-        onBackdropPress={() => {
-          setIsDialogVisible(false);
-        }}
+        onBackdropPress={() => setIsAddDialogVisible(false)}
       >
         <Dialog.Title>Créer une tâche</Dialog.Title>
         <Dialog.Description>
@@ -104,7 +149,7 @@ export default function App() {
         <Dialog.Input onChangeText={setInputValue} />
         <Dialog.Button
           disabled={inputValue.trim().length === 0}
-          label="créer"
+          label="Créer"
           onPress={addTodo}
         />
       </Dialog.Container>
