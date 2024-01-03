6d45b6c59771c2b382e771d2d210605f5af7abc9
Sonia CHELLABI <chellabisonia@gmail.com>
2024-01-03 11:49:14 +0100

    CardTodo component

--- react-native-todolist/react-native-todolist/App.js
+++ react-native-todolist/react-native-todolist/App.js
@@ -1,7 +1,22 @@
-import { Text, View } from "react-native";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { s } from "./App.style";
 import { Header } from "./components/Header/Header";
+import { CardTodo } from "./components/CardTodo/CardTodo";
+import { useState } from "react";
+import { ScrollView, Text, View } from "react-native";
+
+
+const TODO_LIST = [
+  { id: 1, title: "Sortir le chien", isCompleted: true },
+  { id: 2, title: "Aller chez le garagiste", isCompleted: false },
+  { id: 3, title: "Faire les courses", isCompleted: true },
+  { id: 4, title: "Appeler le vétérinaire", isCompleted: true },
+  { id: 5, title: "Sortir le chien", isCompleted: true },
+  { id: 6, title: "Aller chez le garagiste", isCompleted: false },
+  { id: 7, title: "Faire les courses", isCompleted: true },
+  { id: 8, title: "Appeler le vétérinaire", isCompleted: true },
+];
+
 export default function App() {
   return (
     <>
@@ -11,7 +26,7 @@ export default function App() {
             <Header />
           </View>
           <View style={s.body}>
-            <Text>Body</Text>
+            <CardTodo todo={TODO_LIST[0]} />
           </View>
         </SafeAreaView>
       </SafeAreaProvider>
--- react-native-todolist/react-native-todolist/App.style.js
+++ react-native-todolist/react-native-todolist/App.style.js
@@ -4,12 +4,12 @@ export const s = StyleSheet.create({
   app: {
     backgroundColor: "#F9F9F9",
     flex: 1,
-    padding:10,
+    padding: 10,
   },
-  header: { flex: 1,
- },
-  body: { flex: 5,
-    },
-  footer: { height: 70,
- },
+
+  header: { flex: 1 },
+
+  body: { flex: 5 },
+
+  footer: { height: 70 },
 });
